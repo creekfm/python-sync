ef28689b4a36d22d1ee6a4f89a7054d0223f613d
Travis Bernard <travis@creek.fm>
2015-02-02 20:21:22 -0800

    remove init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-echo "# python-sync" >> README.md
-git init
-git add README.md
-git commit -m "Initial commit"
-git remote add origin https://github.com/creekfm/python-sync.git
-git push -u origin master
